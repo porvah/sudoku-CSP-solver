bf39d1914ba701ba10676768c25adf032eb4aa75
Porvah <3msabdullah@gmail.com>
2024-12-21 01:54:04 +0200

    started generator

--- logic/problemMaker.py
+++ logic/problemMaker.py
@@ -2,4 +2,12 @@ from logic.solver import Solver
 
 
 class problemMaker(Solver):
+    def __init__(self):
+        empty_grid = [[0 for _ in range(9)] for _ in range(9)]
+        super().__init__(empty_grid)
+        self.solve() # gets a valid solution
+
+    
+
+    
     
